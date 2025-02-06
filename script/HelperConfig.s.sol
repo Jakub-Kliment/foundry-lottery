d45c883c28cd3d059a585a57bbcaa9a88a535276
Jakub-Kliment <kubo.kliment4@gmail.com>
2025-02-06 11:49:27 +0100

    Finished scripts and continued with tests

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
+import {LinkToken} from "test/mocks/LinkToken.sol";
 
 abstract contract CodeConstants {
     // VRF Mock value
@@ -16,7 +17,6 @@ abstract contract CodeConstants {
 contract HelperConfig is CodeConstants, Script {
 
     error HelperConfig__InvalidChainId();
-
     struct NetworkConfig {
         uint256 entranceFee;
         uint256 interval;
@@ -24,6 +24,7 @@ contract HelperConfig is CodeConstants, Script {
         bytes32 gasLane;
         uint32 callbackGasLimit;
         uint256 subscriptionId;
+        address link; 
     }
 
     NetworkConfig public localNetworkConfig;
@@ -50,7 +51,8 @@ contract HelperConfig is CodeConstants, Script {
             vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
             gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
             callbackGasLimit: 500000,
-            subscriptionId: 0
+            subscriptionId: 104202172772455531254058573566846635159544854406470292625005004022680360261368,
+            link: 0x779877A7B0D9E8603169DdbD7836e478b4624789
         });
     }
 
@@ -64,6 +66,8 @@ contract HelperConfig is CodeConstants, Script {
                 MOCK_GAS_PRICE_LINK,
                 MOCK_WEI_PER_UNIT_LINK  
             );
+            LinkToken linkToken = new LinkToken();
+
             vm.stopBroadcast();
 
             localNetworkConfig = NetworkConfig({
@@ -72,7 +76,8 @@ contract HelperConfig is CodeConstants, Script {
                 vrfCoordinator: address(vrfCoordinatorMock),
                 gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
                 callbackGasLimit: 500000,
-                subscriptionId: 0
+                subscriptionId: 0,
+                link: address(linkToken) 
             });
             return localNetworkConfig;
         }
